3a6e1efa237e3e657d94425dccc5f90606b4ca58
Soumya Datta <69461309+dattasoumya92@users.noreply.github.com>
2021-01-28 23:08:51 +0530

    Create props.js

---
+++ props.js
@@ -0,0 +1,7 @@
+/** PROVIDE INPUT BELOW */
+
+const apikey = 'Bearer <api_key>'
+const gfhostip = '<your_grafana_host_ip_or_domain_name>'
+const gfport = '<grafana_port>'
+const gfSubPath = '<sub_path_value_from_grafana_ini_properties>'
+const httpsEnabled = false // whether https is enabled on grafana server
